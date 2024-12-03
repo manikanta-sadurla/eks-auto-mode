02393fd8a75e359b00352818e635b21d5f563b94
Manikanta <manikanta.sadurla@sourcefuse.com>
2024-12-03 15:12:40 +0530

    module

--- module/provider.tf
+++ module/provider.tf
@@ -21,6 +21,10 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
