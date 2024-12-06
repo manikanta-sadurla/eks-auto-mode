e63e8000de5a62da15120b52c3d80dd9dcee6329
Manikanta <manikanta.sadurla@sourcefuse.com>
2024-12-06 17:55:10 +0530

    changes

--- rabbitmq-module/data.tf
+++ rabbitmq-module/data.tf
@@ -2,18 +2,18 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
 
-# data "aws_vpc" "selected" {
-#   id = var.vpc_id
-# }
+data "aws_vpc" "selected" {
+  id = var.vpc_id
+}
 
-# data "aws_subnets" "private" {
-#   filter {
-#     name   = "vpc-id"
-#     values = [var.vpc_id]
-#   }
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [var.vpc_id]
+  }
 
-#   filter {
-#     name   = "tag:Tier"
-#     values = ["private"]
-#   }
-# }
+  filter {
+    name   = "tag:Tier"
+    values = ["private"]
+  }
+}
