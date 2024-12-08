e7338718a46455b720b7e4d176c5560a849a1323
Manikanta <manikanta.sadurla@sourcefuse.com>
2024-12-08 13:19:58 +0530

    changes

--- module/provider.tf
+++ module/provider.tf
@@ -25,24 +25,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "kubernetes" {
+#   host                   = data.aws_eks_cluster.eks.endpoint
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#   token                  = data.aws_eks_cluster_auth.eks.token
+# }
 
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks.token
-  }
-}
+# provider "helm" {
+#   kubernetes {
+#     host                   = data.aws_eks_cluster.eks.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks.token
+#   }
+# }
 
-data "aws_eks_cluster" "eks" {
-  name = aws_eks_cluster.example.id
-}
+# data "aws_eks_cluster" "eks" {
+#   name = aws_eks_cluster.example.id
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = aws_eks_cluster.example.id
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = aws_eks_cluster.example.id
+# }
