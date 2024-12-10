ed432fa9189baed9135f2fcaa48d6e4e36e4a1b6
Manikanta <manikanta.sadurla@sourcefuse.com>
2024-12-10 20:07:40 +0530

    changes

--- terraform-off/main.tf
+++ terraform-off/main.tf
@@ -51,19 +51,19 @@ module "eks" {
   vpc_id     = "vpc-0e6c09980580ecbf6"
   subnet_ids = ["subnet-066d0c78479b72e77", "subnet-064b80a494fed9835"]
 
-   eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2_x86_64"
-      instance_types = ["m6i.large"]
+#    eks_managed_node_groups = {
+#     example = {
+#       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+#       ami_type       = "AL2_x86_64"
+#       instance_types = ["m6i.large"]
 
-      min_size = 2
-      max_size = 5
-      # This value is ignored after the initial creation
-      # https://github.com/bryantbiggs/eks-desired-size-hack
-      desired_size = 2
-    }
-  }
+#       min_size = 2
+#       max_size = 5
+#       # This value is ignored after the initial creation
+#       # https://github.com/bryantbiggs/eks-desired-size-hack
+#       desired_size = 2
+#     }
+#   }
 
   tags = {
     Test       = "Created by Terraform-REG"
